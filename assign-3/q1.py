d46f0362c8e78ca6d58fb3eb559e59ae9f141d15
Varun Sundar <varun19299@gmail.com>
2018-10-28 23:48:06 +0530

    added assign 3$

--- assign-3/q1.py
+++ assign-3/q1.py
@@ -43,7 +43,7 @@ plt.show()
 train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
 
 # convert test data into Variable, pick 2000 samples to speed up testing
-test_data = dsets.MNIST(root='./mnist/', train=False, transform=transforms.ToTensor())
+test_data = dsets.MNIST(root='/tmp/mnist/', train=False, transform=transforms.ToTensor())
 test_x = test_data.test_data.type(torch.FloatTensor)[:2000]/255.   # shape (2000, 28, 28) value in range(0,1)
 test_y = test_data.test_labels.numpy().squeeze()[:2000]    # covert to numpy array
 
